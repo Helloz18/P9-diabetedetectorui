6680718103b09719f1e9e29bf915e01a4554d998
Héloïse Finoux-Nikolic <hf2810@hotmail.com>
2021-06-10 22:38:14 +0200

    add new history ok

--- src/app/components/history-create/history-create.component.ts
+++ src/app/components/history-create/history-create.component.ts
@@ -1,4 +1,10 @@
+import { HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ApiHistoryService } from 'src/app/service/api-history.service';
 
 @Component({
   selector: 'app-history-create',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryCreateComponent implements OnInit {
 
-  constructor() { }
+  historyCreateForm: FormGroup;
+  id: number;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiHistoryService: ApiHistoryService,
+    private router: Router,
+    private route: ActivatedRoute
+  
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe( params => { this.id = +params['patId']} );
+    
+    this.historyCreateForm = this.formBuilder.group({
+      noteTitle: '',
+      doctorName: '',
+      noteDate: '',
+      noteContent:''
+    });
+
   }
 
+  createHistory() {
+      const formValue = this.historyCreateForm.value;
+      console.log(formValue);
+      let createdNote = {
+        noteTitle: formValue['noteTitle'],
+        doctorName: formValue['doctorName'],
+        noteDate: formValue['noteDate'],
+        noteContent: formValue['noteContent']  
+      };    
+      let createdHistory = {     
+        patId: this.id,
+        note: createdNote
+      }  
+        this.apiHistoryService.addHistory(createdHistory).subscribe(
+          result => {
+            if (result.type == HttpEventType.Response) {
+              this.router.navigate(['/history/'+this.id]);
+            }
+          },
+          error => {
+            console.log(error);
+          }
+        );
+    }
+  
+
+  
 }
